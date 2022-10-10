7763941932d20e7907ec9a586c887689f3f8d96e
alvesgf16 <alvesgabriel1994@gmail.com>
2022-10-10 13:24:09 -0300

    feat: base game

--- programacao/python/games/guessing.py
+++ programacao/python/games/guessing.py
@@ -1,1 +1,16 @@
+print("*****************************")
 print("Welcome to the Guessing game!")
+print("*****************************")
+
+secret_number = 42
+
+guess_str = input("Enter your number: ")
+
+print("You entered", guess_str)
+
+guess = int(guess_str)
+
+if secret_number == int(guess):
+    print("You're right!")
+else:
+    print("You're wrong...")
