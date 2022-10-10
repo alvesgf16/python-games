4202c96bfa9908335b2385faebc4e58a787184ec
alvesgf16 <alvesgabriel1994@gmail.com>
2022-10-10 13:49:13 -0300

    feat: game rounds

--- programacao/python/games/guessing.py
+++ programacao/python/games/guessing.py
@@ -3,21 +3,27 @@ print("Welcome to the Guessing game!")
 print("*****************************")
 
 secret_number = 42
+total_rounds = 3
+round_number = 1
 
-guess_str = input("Enter your guess: ")
-print("You entered", guess_str)
-guess = int(guess_str)
+while round_number <= total_rounds:
+    print(f"Round {round_number} of {total_rounds}")
+    guess_str = input("Enter your guess: ")
+    print("You entered", guess_str)
+    guess = int(guess_str)
 
-hit = guess == secret_number
-bigger = guess > secret_number
-smaller = guess < secret_number
+    hit = guess == secret_number
+    bigger = guess > secret_number
+    smaller = guess < secret_number
 
-if hit:
-    print("You're right!")
-else:
-    if bigger:
-        print("You missed! Your guess was bigger than the secret number.")
-    elif smaller:
-        print("You missed! Your guess was smaller than the secret number.")
+    if hit:
+        print("You're right!")
+    else:
+        if bigger:
+            print("You missed! Your guess was bigger than the secret number.")
+        elif smaller:
+            print("You missed! Your guess was smaller than the secret number.")
+
+    round_number += 1
 
 print("Game over")
