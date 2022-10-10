8f35cff8fe97e9a41c8459f6384e5da85da8a48a
alvesgf16 <alvesgabriel1994@gmail.com>
2022-10-10 14:03:11 -0300

    refactor: replace while-loop with for-loop

--- programacao/python/games/guessing.py
+++ programacao/python/games/guessing.py
@@ -4,13 +4,16 @@ print("*****************************")
 
 secret_number = 42
 total_rounds = 3
-round_number = 1
 
-while round_number <= total_rounds:
+for round_number in range(1, total_rounds + 1):
     print(f"Round {round_number} of {total_rounds}")
-    guess_str = input("Enter your guess: ")
+    guess_str = input("Enter a number between 1 and 100: ")
     print("You entered", guess_str)
     guess = int(guess_str)
+
+    if guess < 1 or guess > 100:
+        print("You must enter a number between 1 and 100!")
+        continue
 
     hit = guess == secret_number
     bigger = guess > secret_number
@@ -18,12 +21,11 @@ while round_number <= total_rounds:
 
     if hit:
         print("You're right!")
+        break
     else:
         if bigger:
             print("You missed! Your guess was bigger than the secret number.")
         elif smaller:
             print("You missed! Your guess was smaller than the secret number.")
-
-    round_number += 1
 
 print("Game over")
