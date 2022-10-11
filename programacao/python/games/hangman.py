9b3cd3866555571a7c5b7136767989b55c2672e8
alvesgf16 <alvesgabriel1994@gmail.com>
2022-10-11 09:56:03 -0300

    feat: base hangman game

---
+++ programacao/python/games/hangman.py
@@ -0,0 +1,18 @@
+def play():
+    print("****************************")
+    print("Welcome to the Hangman game!")
+    print("****************************")
+
+    secret_word = "banana"
+
+    hanged = False
+    guessed = False
+
+    while not hanged and not guessed:
+        print('Playing...')
+
+    print("Game over")
+
+
+if __name__ == "__main__":
+    play()
